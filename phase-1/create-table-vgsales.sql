fd202ed1936eda7881aec6d8a9b7a1bd43ff7e77
Mikkel RINGAUD <mikkel@milescode.dev>
2024-01-12 17:12:43 +0100

    feat: add sql for phase 1

---
+++ phase-1/create-table-vgsales.sql
@@ -0,0 +1,13 @@
+CREATE TABLE VG_SALES (
+  Rank VARCHAR(1024),
+  Name VARCHAR(1024),
+  Platform VARCHAR(1024),
+  Year VARCHAR(1024),
+  Genre VARCHAR(1024),
+  Publisher VARCHAR(1024),
+  NA_Sales VARCHAR(1024),
+  EU_Sales VARCHAR(1024),
+  JP_Sales VARCHAR(1024),
+  Other_Sales VARCHAR(1024),
+  Global_Sales VARCHAR(1024)
+);
